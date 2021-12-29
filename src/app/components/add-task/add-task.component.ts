7076dead4df3f0a359f195ad25c583f28eead1e2
Oleg Vinokurov <OlegVinokurov@hotmail.com>
2021-12-28 22:06:40 -0800

    add task component show or hide

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 import {Task} from '../../Task';
 
 @Component({
@@ -13,8 +15,14 @@ export class AddTaskComponent implements OnInit {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription: Subscription;
   
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value)=>(this.showAddTask=value))
+   }
 
   ngOnInit(): void {}
 
