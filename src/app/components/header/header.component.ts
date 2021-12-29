fbf31c0112924cef03857072744134d0401e4c41
Oleg Vinokurov <OlegVinokurov@hotmail.com>
2021-12-28 21:58:07 -0800

    Service for ADD/CLOSE button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
+import { UiService } from './../../services/ui.service';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  constructor() { }
+  showAddTask :boolean = false;
+  subscription!:Subscription;
+
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value)=>(this.showAddTask=value))
+   }
 
   ngOnInit(): void {}
 
   toggleAddTask(){
-    console.log('Toggle')
+    this.uiService.toggleAddTask();
   }
 
 }
